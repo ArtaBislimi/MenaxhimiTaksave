d99a72a86f6b7324f272e806f45b4a9ef4ba40b1
ArtaBislimi <63665734+ArtaBislimi@users.noreply.github.com>
2020-07-18 19:08:26 +0200

    Add files via upload

---
+++ App_Code/UserSession.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MenaxhimiTaksit.BO;
+
+namespace MenaxhimiTaksit.App_Code
+{
+    public class UserSession
+    {
+        public static User LoggedUser = null;
+        
+        //Metoda per Manual
+        public static void ShowHelpManual(System.Windows.Forms.Form parent ,string htmlpage)
+        {                                              //linku i manualit
+            System.Windows.Forms.Help.ShowHelp(parent, @" C:\Users\Arta\Dropbox\My PC (DESKTOP-496EP8N)\Desktop\MenaxhimiTaksitManual.chm", System.Windows.Forms.HelpNavigator.TableOfContents,htmlpage);// helpNavigation.Topic //forma:overview
+        }
+    }
+}
